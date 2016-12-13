35ddea556daaed789567949cc3811b60fb36cd64
bhavikpatelv <bhavikpatelv@gmail.com>
2016-12-13 02:14:22 -0800

    Added missing fields to UI and initial Regular Expressions to extract important data

--- AutoCheckin/MainWindow.xaml.cs
+++ AutoCheckin/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,10 +25,11 @@ namespace AutoCheckin
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string tessaData = @"C:\Users\Bhavik Patel\Documents\visual studio 2015\Projects\AutoCheckin\AutoCheckin\tessdata";
         public MainWindow()
         {
             InitializeComponent();
-       } 
+        }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
@@ -35,14 +37,35 @@ namespace AutoCheckin
             myFile.InitialDirectory = "c:\\";
             if (myFile.ShowDialog() == true)
             {
-                image1.Source =  new BitmapImage(new Uri(myFile.FileName));
+                Bitmap mybmp = new Bitmap(myFile.FileName);
+                image1.Source = new BitmapImage(new Uri(myFile.FileName));
+                var ocr = new TesseractEngine(tessaData, "eng", EngineMode.Default);
+                var page = ocr.Process(mybmp, PageSegMode.Auto);
+                var text = page.GetText();
+                var output = text.Substring(text.IndexOf('"')+1);
+                output = Regex.Replace(output, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+                char[] RemoveList = { ',', ' ' };
+                Regex regexLC = new Regex(@"[A-Z0-9]*,");
+                Regex regexFS = new Regex(@", [A-Z0-9]*");
+                Regex regexA = new Regex(@"\n(.*?)*\n");
+                Regex regexZ = new Regex(@", [A-Z][A-Z] \d{5}");
+
+                var address = regexA.Match(output).ToString().TrimStart();
+                var lastCity = regexLC.Matches(output).Cast<Match>().ToList();
+                var firstState = regexFS.Matches(output).Cast<Match>().ToList();
+                var firstName = firstState.First().ToString().TrimStart(RemoveList);
+                var state = firstState.Last().ToString().TrimStart(RemoveList);
+                var lastName = lastCity.First().ToString().TrimEnd(RemoveList);
+                var city = lastCity.Last().ToString().TrimEnd(RemoveList);
+                var zipCode = regexZ.Match(output).ToString().Substring(5);
+
+                lastNameTextBox.Text = lastName;
+                firstNameTextBox.Text = firstName;
+                addressTextBox.Text = address;
+                cityTextBox.Text = city;
+                stateTextBox.Text = state;
+                zipCodeTextBox.Text = zipCode;
             }
-            Bitmap bmpImage = new Bitmap(myFile.FileName);
-            var ocr = new TesseractEngine(@"C:\Users\Bhavik Patel\Documents\visual studio 2015\Projects\AutoCheckin\AutoCheckin\tessdata", "eng", EngineMode.Default);
-            var page = ocr.Process(bmpImage, PageSegMode.Auto);
-            var text = page.GetText();
-            FileInfo fi = new FileInfo(myFile.FileName);
-            nameTextBox.Text = text;
         }
     }
 }
